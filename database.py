c90b9feba630833a763bf328a83f506781f4cc94
Parth Sarthi <parthsarthi2719@gmail.com>
2026-03-13 16:57:26 +0530

    add seed data with  GRE questions

--- database.py
+++ database.py
@@ -1,10 +1,14 @@
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
+import certifi
 
 load_dotenv()
 
-client = MongoClient(os.getenv("MONGODB_URI"))
+client = MongoClient(
+    os.getenv("MONGODB_URI"),
+    tlsCAFile=certifi.where()
+)
 db = client["adaptive_engine"]
 
 questions_collection = db["questions"]
--- seed.py
+++ seed.py
@@ -1,3 +1,6 @@
+from dotenv import load_dotenv
+load_dotenv()
+
 from database import questions_collection
 import uuid
 
